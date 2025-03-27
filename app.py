fc4b6d8832846d040bba89c3a5a026a608368732
paexternal293 <paexternal293@oaisb.info>
2025-03-27 11:12:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ def is_prime(n):
     if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+        if n % i == 0
+                            print(f"Divisor found: {i}"):
             return False
     return True
 
